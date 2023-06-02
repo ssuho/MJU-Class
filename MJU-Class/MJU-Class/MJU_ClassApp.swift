83584fc9bf6ca7a51c6460c1731ae67231829a3a
ssuho <devsuhokim@gmail.com>
2023-06-02 13:51:23 +0900

    새 프로젝트 생성

---
+++ MJU-Class/MJU-Class/MJU_ClassApp.swift
@@ -0,0 +1,17 @@
+//
+//  MJU_ClassApp.swift
+//  MJU-Class
+//
+//  Created by SUHO KIM on 2023/06/02.
+//
+
+import SwiftUI
+
+@main
+struct MJU_ClassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
